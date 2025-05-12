70d78cee2c1e815c5f4808e11614e8ef08d13b0e
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-12 21:24:36 +0500

    sff

--- filter_task_4_1.py
+++ filter_task_4_1.py
@@ -1,4 +1,4 @@
-def filter_task_4_1(numbers):
+def filter_task_4_1(n):
     """
     Filter out even numbers.
     Args:
@@ -6,4 +6,7 @@ def filter_task_4_1(numbers):
     Returns:
         list of odd numbers
     """
-    return
+    n=[1,2,3,4,5,6,7,8,9,10]
+    a=filter(lambda n: n%2==1,n)
+    return list(a)
+print(filter_task_4_1(10)) 
