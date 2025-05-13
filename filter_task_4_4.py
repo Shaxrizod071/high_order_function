b1ffb4b2de293c504cfc16b07b6851c8991bd275
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-13 10:47:31 +0500

    skjb

--- filter_task_4_4.py
+++ filter_task_4_4.py
@@ -1,4 +1,4 @@
-def filter_task_4_4(names):
+def filter_task_4_4(n):
     """
     Filter names that start with "A".
     Args:
@@ -6,4 +6,8 @@ def filter_task_4_4(names):
     Returns:
         list of names that start with "A"
     """
-    return
+    return n[0]
+    n=["Alice","Bob","Anna","Charline"]
+    s=filter(lambda x: x[0] if x[0]==A or x[0]==a else n,n)
+    return list(s)
+print(filter_task_4_4("Alice"))
