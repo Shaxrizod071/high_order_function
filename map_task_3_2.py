86f1682a4e823d397e3ed645c631dc5cfcb79662
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-14 00:24:23 +0500

    akd

--- map_task_3_2.py
+++ map_task_3_2.py
@@ -1,4 +1,4 @@
-def map_task_3_2(words):
+def map_task_3_2(w):
     """
     Convert all words to uppercase.
     Args:
@@ -6,4 +6,6 @@ def map_task_3_2(words):
     Returns:
         list of uppercase words
     """
-    return
+    x=map(lambda x: x.title(),w)
+    return list(x)
+print(map_task_3_2(["cat","dog"]))
