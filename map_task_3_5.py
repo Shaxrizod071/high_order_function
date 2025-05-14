1274a71214c27168474ff4493d7785b4b17b8310
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-14 22:32:05 +0500

    cdc

--- map_task_3_5.py
+++ map_task_3_5.py
@@ -1,4 +1,7 @@
-def map_task_3_5(people):
+
+    """
+    return (list(map(lambda x: x[1],p))
+)def map_task_3_5(p):
     """
     Extract names from a list of dictionaries.
     Args:
@@ -6,4 +9,5 @@ def map_task_3_5(people):
     Returns:
         list of names
     """
-    return
+    return (list(map(lambda x: x[1],p)))
+print(map_task_3_5([{"name": "Alice"}, {"name": "Bob"}, {"name": "Carol"}))
