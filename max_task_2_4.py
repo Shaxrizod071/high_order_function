949198c975b27ada8779556a06fc048f8b770996
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-15 15:58:57 +0500

    akbf

--- max_task_2_4.py
+++ max_task_2_4.py
@@ -6,9 +6,12 @@ def max_task_2_4(w):
     Returns:
         word with most vowels
     """
-    if w in 'aieuo':
-         return True
-    else:
-        return False
+    def name(s):
+        x=0
+        for i in s:
+            if i in 'aoiue':
+                x+=1
+        return x
+    return (max(w,key=lambda x: name(x)))
 print(max_task_2_4(["tree", "education", "sky", "road"]))
 
